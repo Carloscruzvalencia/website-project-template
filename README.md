0a3d921dd754e66a89bfb8870a819fd5cab25995
Carlos cruz valencia <a2carcruzinfo@gmail.com>
2022-08-07 17:55:32 +0200

    \update\

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
-# Titulo del proyecto
+# {pTitle}
 
 ## Inpiracion del proyecto & objetivo
 
--  
+-  {pDesc}
 
 El diseño web se ha creado en [Figma](https://www.figma.com/file/kP0SJhf4iDDa9kAzsz1LM1/Github-projects?node-id=0%3A1) por Carlos cruz valencia
+
+Puedes visitar el proyecto en [Githubpages]({pUrl})
+
+## Links de interes
+<!-- link a correo -->
+<code> <a href="mailto:a2carcruzinfo@gmail.com"><img height="25" src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white"></a></code>
+<code><a a href="https://www.figma.com/file/kP0SJhf4iDDa9kAzsz1LM1/Github-projects?node-id=0%3A1"><img height="25" src="https://img.shields.io/badge/Figma-F24E1E?style=for-the-badge&logo=figma&logoColor=white"></a></code>
+<code><a a href="https://www.linkedin.com/in/carlos-cruz-valencia-564514162/"><img height="25" src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white"></a></code>
 
 ## Tecnologias usadas
 
@@ -13,90 +21,23 @@ El diseño web se ha creado en [Figma](https://www.figma.com/file/kP0SJhf4iDDa9k
     <code><img height="25" src="https://img.shields.io/badge/Visual_Studio_Code-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white"></code>
 
 - Programas usados
-    <!-- figma -->
-    <code><img height="30" src="https://img.shields.io/badge/Figma-F24E1E?style=for-the-badge&logo=figma&logoColor=white"></code><!-- gitkraken -->
-    <code><img height="30" src="https://img.shields.io/badge/GitKraken-179287?style=for-the-badge&logo=GitKraken&logoColor=white"></code><!-- Xamp -->
-    <code><img height="30" src="https://img.shields.io/badge/Xampp-F37623?style=for-the-badge&logo=xampp&logoColor=white"></code><!-- trello -->
-    <code><img height="30" src="https://img.shields.io/badge/Trello-0052CC?style=for-the-badge&logo=trello&logoColor=white"></code><!-- my sql -->
-    <code><img height="40" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mysql/mysql.png"></code>
+    <code><img height="30" src="https://img.shields.io/badge/Figma-F24E1E?style=for-the-badge&logo=figma&logoColor=white"></code>
 - lenguajes/frameworks usados
-    <!-- bootstrap -->
-    <code><img height="30" src="https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white"></code><!-- html -->
-    <code><img height="30" src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white"></code><!-- css -->
-    <code><img height="30" src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white"></code><!-- python -->
-    <code><img height="30" src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white"></code><!-- sass -->
-    <code><img height="30" src="https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white"></code><!-- javascript -->
-    <code><img src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E"></img></code><!-- srolltrigger -->
-    <code><img src="https://camo.githubusercontent.com/12c4402a62af1a832fe34f8f60d5494706dd433de5fe65c472d6fa66f0f5578e/687474703a2f2f677265656e736f636b2e636f6d2f5f696d672f6769746875622f7468756d622d7363726f6c6c747269676765722d736d616c6c2e676966" height="30"></img></code><!-- textillate -->
-    <code><img src="https://m1.paperblog.com/i/170/1706558/textillatejs-animaciones-texto-usando-css3-L-RwyQR7.jpeg" height="32"></img></code><!-- animate -->
-    <code><img src="https://www.drupaladicto.com/sites/drupaladicto/files/inline-images/pagina%20ofical%20animate%20css.PNG" height="32"></img></code>
- <code><img src="https://i.postimg.cc/0QLfkqmD/pocoo-flask-ar21.png)](https://postimg.cc/jw4NcmMS" height="30" width="90"></img></code>
+
+{pLang}
 
 ## Vista previa del proyecto
 
-``vista no disponible``
-<!-- <img src="project-preview.png" aling="center"></img> -->
-<!-- <img src="project-preview.gif" aling="center"></img> -->
+{pPreview}
 
 ## Estado del proyecto
 
-|Trabajando en el proyecto|✔️|
+|Trabajando en el proyecto|{st1}|
 | -------------------------- | :----------------: |
-|            Web responsive  |      ❌        |
-|           Hosting          |    |
-| Websever app online        |         ❌    |  
-| Dispone de trello          |         ❌    |  
-
-<details >
-<summary>⚙️ documentacion del proyecto ⚙️</summary>
-
-1. Creacion de la pagina web
-    ![]()
-
-</details>
-
-<details >
-<summary>📁 Estructura de carpetas 📁</summary>
-
-```text
-/
-├── README.md
-├── Licence
-├── index.html
-└── assets/
-    ├── js/
-    │   └── app.js
-    └── media/
-    │    ├── img/
-    │    │   └── components/
-    │    │       ├── footer/
-    │    │       └── header/
-    │    └── layouts/
-    │    │   └── landing/
-    │    │   
-    │    │       
-    │    └── video/
-    │
-    │   
-    └── styles/
-        ├── css/
-        │   ├── main.css
-        │   └── main.css.map
-        └── sass/
-            ├── base/
-            │   └──_settings.scss
-            ├── components/
-            │   └──_menu.scss
-            ├── layouts/
-            │   └──_landing.scss
-            └── main.scss
-```
-
-</details>
-
-
-<!-- └── / ├── │ -->
-
+| Web responsive          |{st2}|
+| Hosting                 |{st3}|
+| Hosting disponible      |{st4}|  
+| Dispone de trello       |{st5}| 
 
 ## Licencia
 
@@ -110,7 +51,7 @@ git add -A && git commit -a -m \"update\" && git push
 
 ## sass compiler code
 ```shell
-sass -w --style compressed assets/styles/sass/main.scss assets/styles/css/main.css
+sass -w --style compressed static/styles/sass/main.scss static/styles/css/main.css
 ``` -->
 
 <!-- emojis  -->
--- Readme-gen.py
+++ Readme-gen.py
@@ -2,106 +2,106 @@ from moviepy.editor import VideoFileClip
 from os import system
 import webbrowser
 
-# with open('test.md', 'r') as file :
-#   filedata = file.read()
+with open('test.md', 'r') as file :
+  filedata = file.read()
 
 pTitle = input("Nombre del proyecto: ") # Nombre del proyecto
-# filedata = filedata.replace("{pTitle}",pTitle)
+filedata = filedata.replace("{pTitle}",pTitle)
 
-# pDesc = input("Descripción del proyecto: ") # Descripción del proyecto
-# filedata = filedata.replace('{pDesc}', pDesc)
+pDesc = input("Descripción del proyecto: ") # Descripción del proyecto
+filedata = filedata.replace('{pDesc}', pDesc)
 
-# pUrl = "https://carloscruzvalencia.github.io/" + pTitle # Url del proyecto
-# filedata = filedata.replace('{pDesc}', pUrl)
+pUrl = "https://carloscruzvalencia.github.io/" + pTitle # Url del proyecto
+filedata = filedata.replace('{pUrl}', pUrl)
 
-# # #lenguajes del proyecto
+# #lenguajes del proyecto
 
-# html = "<code><img height=\"30\" src=\"https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white\"></code>"
-# css = "<code><img height=\"30\" src=\"https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white\"></code>"
-# sass = "<code><img height=\"30\" src=\"https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white\"></code>"
-# javascript = "<code><img height=\"30\" src=\"https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E\"></img></code>"
-# python = "<code><img height=\"30\" src=\"https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white\"></code>"
+html = "<code><img height=\"30\" src=\"https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white\"></code>"
+css = "<code><img height=\"30\" src=\"https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white\"></code>"
+sass = "<code><img height=\"30\" src=\"https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white\"></code>"
+javascript = "<code><img height=\"30\" src=\"https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E\"></img></code>"
+python = "<code><img height=\"30\" src=\"https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white\"></code>"
 
-# list = ["html", "css", "sass", "javascript", "python"]
+list = ["html", "css", "sass", "javascript", "python"]
 
-# sel =   True
+sel =   True
 
-# n = ""
-# while sel == True:
-#     print(list)
-#     print(n)
-#     lang = input("seleciona una opcion: ")
-#     if lang == "0":
-#         n = html 
-#     if lang == "1":
-#         n = css
-#     if lang == "2":
-#         n = sass
-#     if lang == "3":
-#         n = javascript
-#     if lang == "4":
-#         n = python
-#     if lang == "5":
-#         sel = False
-#         filedata = filedata.replace('{pLang}',"")
+n = ""
+while sel == True:
+    print(list)
+    print(n)
+    lang = input("seleciona una opcion: ")
+    if lang == "0":
+        n = html 
+    if lang == "1":
+        n = css
+    if lang == "2":
+        n = sass
+    if lang == "3":
+        n = javascript
+    if lang == "4":
+        n = python
+    if lang == "5":
+        sel = False
+        filedata = filedata.replace('{pLang}',"")
 
-#     filedata = filedata.replace('{pLang}', n + "{pLang}")
-#     with open('test.md', 'w') as file:
-#         file.write(filedata)
+    filedata = filedata.replace('{pLang}', n + "{pLang}")
+    with open('test.md', 'w') as file:
+        file.write(filedata)
 
-# #vista previa del proyecto
+#vista previa del proyecto
 
-# print("1 si no tienes imagen de preview")
-# print("2 si tienes imagen de preview")
-# print("3 si tienes gif de preview")
-# print("4 si tienes mp4 de preview")
+print("1 si no tienes imagen de preview")
+print("2 si tienes imagen de preview")
+print("3 si tienes gif de preview")
+print("4 si tienes mp4 de preview")
 
-# preview = input("seleciona una opcion: ")
+preview = input("seleciona una opcion: ")
 
-# if preview == "1":
-#     filedata = filedata.replace('{pPreview}',"``vista no disponible``")
-# if preview == "2":
-#     filedata = filedata.replace('{pPreview}', "![preview](./preview.png)")
-# if preview == "3":
-#     filedata = filedata.replace('{pPreview}', "![preview](./preview.gif)")
-# if preview == "4":
-#     videoClip = VideoFileClip("preview.mp4")
-#     videoClip.write_gif("project-preview.gif")
-#     filedata = filedata.replace('{pPreview}', "![preview](./project-preview.gif)")
+if preview == "1":
+    filedata = filedata.replace('{pPreview}',"``vista no disponible``")
+if preview == "2":
+    filedata = filedata.replace('{pPreview}', "![preview](./preview.png)")
+if preview == "3":
+    filedata = filedata.replace('{pPreview}', "![preview](./preview.gif)")
+if preview == "4":
+    videoClip = VideoFileClip("preview.mp4")
+    videoClip.write_gif("project-preview.gif")
+    filedata = filedata.replace('{pPreview}', "![preview](./project-preview.gif)")
 
-# #project status
+#project status
 
-# status = input("estas Trabajando en el proyecto: ")
-# if status == "si":
-#     filedata = filedata.replace('{st1}', ":heavy_check_mark:")
-# else:
-#     filedata = filedata.replace('{st1}', ":x:")
-# status = input("es responsive: ")
-# if status == "si":
-#     filedata = filedata.replace('{st2}', ":heavy_check_mark:")
-# else:
-#     filedata = filedata.replace('{st2}', ":x:")
-# status = input("tiene hosting: ")
-# if status == "si":
-#     filedata = filedata.replace('{st3}', "Githubpages")
-# elif status == "otro":
-#     host = input("ingresa el hosting: ")
-#     filedata = filedata.replace('{st3}', host)
-# else:
-#     filedata = filedata.replace('{st3}', ":x:")
-# status = input("hosting disponible: ")
-# if status == "si":
-#     filedata = filedata.replace('{st4}', ":heavy_check_mark:")
-# else:
-#     filedata = filedata.replace('{st4}', ":x:")
-# status = input("tiene trello: ")
-# if status == "si":
-#     filedata = filedata.replace('{st5}', ":heavy_check_mark:")
-# else:
-#     filedata = filedata.replace('{st5}', ":x:")
+status = input("estas Trabajando en el proyecto: ")
+if status == "si":
+    filedata = filedata.replace('{st1}', ":heavy_check_mark:")
+else:
+    filedata = filedata.replace('{st1}', ":x:")
+status = input("es responsive: ")
+if status == "si":
+    filedata = filedata.replace('{st2}', ":heavy_check_mark:")
+else:
+    filedata = filedata.replace('{st2}', ":x:")
+status = input("tiene hosting: ")
+if status == "si":
+    filedata = filedata.replace('{st3}', "Githubpages")
+elif status == "otro":
+    host = input("ingresa el hosting: ")
+    filedata = filedata.replace('{st3}', host)
+else:
+    filedata = filedata.replace('{st3}', ":x:")
+status = input("hosting disponible: ")
+if status == "si":
+    filedata = filedata.replace('{st4}', ":heavy_check_mark:")
+else:
+    filedata = filedata.replace('{st4}', ":x:")
+status = input("tiene trello: ")
+if status == "si":
+    filedata = filedata.replace('{st5}', ":heavy_check_mark:")
+else:
+    filedata = filedata.replace('{st5}', ":x:")
 
-# with open('test.md', 'w') as file:
-#     file.write(filedata)
+with open('test.md', 'w') as file:
+    file.write(filedata)
 
 system("git add -A && git commit -a -m \"update\" && git push")
 webbrowser.open("https://github.com/Carloscruzvalencia/"+ pTitle + "#readme")
