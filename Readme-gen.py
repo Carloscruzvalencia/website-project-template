d7bb76a51b49de96ec0dc2f6d5c9f43fddd3206e
Carlos cruz valencia <a2carcruzinfo@gmail.com>
2022-12-26 05:59:07 +0100

    Cambiado el readme-gen.python

--- Readme-gen.py
+++ Readme-gen.py
@@ -1,19 +1,19 @@
 from os import system
 import webbrowser
 
-with open('README.md', 'r') as file :
-  filedata = file.read()
+with open('readme2.md', 'r') as file:
+    filedata = file.read()
 
-pTitle = input("Nombre del proyecto: ") # Nombre del proyecto
-filedata = filedata.replace("{pTitle}",pTitle)
+pTitle = input("Nombre del proyecto: ")
+filedata = filedata.replace("{pTitle}", pTitle)
 
-pDesc = input("Descripción del proyecto: ") # Descripción del proyecto
+pDesc = input("Descripción del proyecto: ")
 filedata = filedata.replace('{pDesc}', pDesc)
 
-pUrl = "https://carloscruzvalencia.github.io/" + pTitle # Url del proyecto
+pUrl = "https://carloscruzvalencia.github.io/" + pTitle
 filedata = filedata.replace('{pUrl}', pUrl)
 
-#lenguajes del proyecto
+
 html = "<code><img height=\"30\" src=\"https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white\"></code>"
 css = "<code><img height=\"30\" src=\"https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white\"></code>"
 sass = "<code><img height=\"30\" src=\"https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white\"></code>"
@@ -23,13 +23,14 @@ scrolltrigger = "<code><img height=\"30\" src=\"https://img.shields.io/badge/Scr
 botstrap = "<code><img height=\"30\" src=\"https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white\"></code>"
 spline = "<code><img height=\"30\" src=\"https://i.postimg.cc/85hR92yS/Group-71.png\"></code>"
 
-list = ["salir = 0", "html = 1", "css =2", "sass =3", "javascript =4", "python =5", "ScrollTrigger =6", "Bootstrap =7", "spline =8"]
-sel =   True
+list = ["salir = 0", "html = 1", "css =2", "sass =3", "javascript =4",
+        "python =5", "ScrollTrigger =6", "Bootstrap =7", "spline =8"]
+sel = True
 while sel == True:
     print(list)
     lang = input("seleciona una opcion: ")
     if lang == "1":
-        n = html 
+        n = html
     elif lang == "2":
         n = css
     elif lang == "3":
@@ -46,26 +47,27 @@ while sel == True:
         n = spline
     else:
         sel = False
-        filedata = filedata.replace('{pLang}',"")
+        filedata = filedata.replace('{pLang}', "")
 
     filedata = filedata.replace('{pLang}', n + "{pLang}")
     with open('README.md', 'w') as file:
         file.write(filedata)
 
-#vista previa del proyecto
 print("1 si no tienes imagen de preview")
 print("2 si tienes imagen de preview")
 print("3 si tienes gif de preview")
 
 preview = input("seleciona una opcion: ")
 if preview == "1":
-    filedata = filedata.replace('{pPreview}',"``vista no disponible``")
+    filedata = filedata.replace('{pPreview}', "``vista no disponible``")
 if preview == "2":
-    filedata = filedata.replace('{pPreview}', "<img src=\"./preview.png\" width=\"100%\">")
+    filedata = filedata.replace(
+        '{pPreview}', "<img src=\"./preview.png\" width=\"100%\">")
 if preview == "3":
-    filedata = filedata.replace('{pPreview}', "<img src=\"./preview.gif\" width=\"100%\">")
+    filedata = filedata.replace(
+        '{pPreview}', "<img src=\"./preview.gif\" width=\"100%\">")
 
-#project status
+
 status = input("estas Trabajando en el proyecto: ")
 if status == "si":
     filedata = filedata.replace('{st1}', ":heavy_check_mark:")
@@ -86,23 +88,7 @@ elif status == "otro":
     filedata = filedata.replace('{st3}', host)
 else:
     filedata = filedata.replace('{st3}', ":x:")
-status = input("hosting disponible: ")
-if status == "si":
-    filedata = filedata.replace('{st4}', ":heavy_check_mark:")
-else:
-    filedata = filedata.replace('{st4}', ":x:")
-status = input("tiene trello: ")
-if status == "si":
-    filedata = filedata.replace('{st5}', ":heavy_check_mark:")
-else:
-    filedata = filedata.replace('{st5}', ":x:")
-
 with open('README.md', 'w') as file:
     file.write(filedata)
 system("DEL Readme-gen.py && git pull && git add -A && git commit -a -m \"update\" && git push")
-webbrowser.open("https://github.com/Carloscruzvalencia/"+ pTitle + "#readme")
-
-
-
-
-
+webbrowser.open("https://github.com/Carloscruzvalencia/" + pTitle + "#readme")
