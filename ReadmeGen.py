284ddc95e757ac464d683401592a6977835caaa2
Carlos cruz valencia <a2carcruzinfo@gmail.com>
2022-12-28 07:15:07 +0100

    Terminadas las modificaciones en el codigo

--- ReadmeGen.py
+++ ReadmeGen.py
@@ -1,7 +1,11 @@
 from os import system
 import webbrowser
+
+
 WARNING = '\033[31m'
 ENDC = '\033[0m'
+
+
 with open('README.md', 'r') as file:
     filedata = file.read()
 pTitle = input("Nombre del proyecto: ")
@@ -10,8 +14,12 @@ pDesc = input("Descripción del proyecto: ")
 filedata = filedata.replace('{pDesc}', pDesc)
 pUrl = "https://carloscruzvalencia.github.io/" + pTitle
 filedata = filedata.replace('{pUrl}', pUrl)
+
+
 print("\n")
 print("Que tecnologias as usado en el proyecto")
+
+
 langUrlArray = ["","<code><img height=\"30\" src=\"https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white\"></code>","<code><img height=\"30\" src=\"https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white\"></code>","<code><img height=\"30\" src=\"https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white\"></code>","<code><img height=\"30\" src=\"https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E\"></img></code>","<code><img height=\"30\" src=\"https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white\"></code>","<code><img height=\"30\" src=\"https://img.shields.io/badge/ScrollTrigger-E34F26?style=for-the-badge&logo=scrolltrigger&logoColor=white\"></code>","<code><img height=\"30\" src=\"https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white\"></code>","<code><img height=\"30\" src=\"https://i.postimg.cc/85hR92yS/Group-71.png\"></code>"]
 langArray = ["salir","html","css","sass","javascript","python","ScrollTrigger","Bootstrap","spline"]
 loop = True
@@ -39,6 +47,8 @@ while loop:
     with open('README.md', 'w') as file:
         file.write(filedata)
 filedata = filedata.replace('{pLang}', "")
+
+
 status = ['Estas trabajando en el proyecto: ','Es responsive: ','Tiene hosting: ']
 while len(status) > 0:
     opt = input(status[0])
@@ -53,6 +63,8 @@ while len(status) > 0:
     status.pop(0)
     with open('README.md', 'w') as file:
         file.write(filedata)
+
+
 print("\n")
 print("Project preview")
 print("1 si no tienes imagen de preview")
@@ -67,6 +79,8 @@ if preview == "2":
 if preview == "3":
     filedata = filedata.replace(
         '{pPreview}', "<img src=\"./preview.gif\" width=\"100%\">")
+
+
 with open('README.md', 'w') as file:
     file.write(filedata)
 system("DEL Readme-gen.py && git pull && git add -A && git commit -a -m \"update\" && git push")
