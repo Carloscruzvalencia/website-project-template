bcf7f749c91176a99874e6e4bc610b1b1401c404
Carlos cruz valencia <a2carcruzinfo@gmail.com>
2022-12-28 06:26:49 +0100

    modificado readme.py cambios aun no terminado

--- Readme-gen.py
+++ Readme-gen.py
@@ -1,84 +1,84 @@
-# from os import system
-# import webbrowser
+from os import system
+import webbrowser
 
-# WARNING = '\033[31m'
-# ENDC = '\033[0m'
+WARNING = '\033[31m'
+ENDC = '\033[0m'
 
 with open('README.md', 'r') as file:
     filedata = file.read()
 
-# # Nombre del proyecto
-# pTitle = input("Nombre del proyecto: ")
-# filedata = filedata.replace("{pTitle}", pTitle)
+# Nombre del proyecto
+pTitle = input("Nombre del proyecto: ")
+filedata = filedata.replace("{pTitle}", pTitle)
 
-# # Descripción del proyecto
-# pDesc = input("Descripción del proyecto: ")
-# filedata = filedata.replace('{pDesc}', pDesc)
+# Descripción del proyecto
+pDesc = input("Descripción del proyecto: ")
+filedata = filedata.replace('{pDesc}', pDesc)
 
-# # Url del proyecto
-# pUrl = "https://carloscruzvalencia.github.io/" + pTitle
-# filedata = filedata.replace('{pUrl}', pUrl)
+# Url del proyecto
+pUrl = "https://carloscruzvalencia.github.io/" + pTitle
+filedata = filedata.replace('{pUrl}', pUrl)
 
 # # lenguajes del proyecto
-# print("\n")
-# print("Que tecnologias as usado en el proyecto")
-# langUrlArray = [
-#     "",
-#     # html
-#     "<code><img height=\"30\" src=\"https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white\"></code>",
-#     # css
-#     "<code><img height=\"30\" src=\"https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white\"></code>",
-#     # sass
-#     "<code><img height=\"30\" src=\"https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white\"></code>",
-#     # javascript
-#     "<code><img height=\"30\" src=\"https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E\"></img></code>",
-#     # python
-#     "<code><img height=\"30\" src=\"https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white\"></code>",
-#     # scrolltringer
-#     "<code><img height=\"30\" src=\"https://img.shields.io/badge/ScrollTrigger-E34F26?style=for-the-badge&logo=scrolltrigger&logoColor=white\"></code>",
-#     # bootsrap
-#     "<code><img height=\"30\" src=\"https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white\"></code>",
-#     # spline
-#     "<code><img height=\"30\" src=\"https://i.postimg.cc/85hR92yS/Group-71.png\"></code>"
-# ]
-# langArray = [
-#     "salir",
-#     "html",
-#     "css",
-#     "sass",
-#     "javascript",
-#     "python",
-#     "ScrollTrigger",
-#     "Bootstrap",
-#     "spline"
-# ]
+print("\n")
+print("Que tecnologias as usado en el proyecto")
+langUrlArray = [
+    "",
+    # html
+    "<code><img height=\"30\" src=\"https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white\"></code>",
+    # css
+    "<code><img height=\"30\" src=\"https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white\"></code>",
+    # sass
+    "<code><img height=\"30\" src=\"https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white\"></code>",
+    # javascript
+    "<code><img height=\"30\" src=\"https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E\"></img></code>",
+    # python
+    "<code><img height=\"30\" src=\"https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white\"></code>",
+    # scrolltringer
+    "<code><img height=\"30\" src=\"https://img.shields.io/badge/ScrollTrigger-E34F26?style=for-the-badge&logo=scrolltrigger&logoColor=white\"></code>",
+    # bootsrap
+    "<code><img height=\"30\" src=\"https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white\"></code>",
+    # spline
+    "<code><img height=\"30\" src=\"https://i.postimg.cc/85hR92yS/Group-71.png\"></code>"
+]
+langArray = [
+    "salir",
+    "html",
+    "css",
+    "sass",
+    "javascript",
+    "python",
+    "ScrollTrigger",
+    "Bootstrap",
+    "spline"
+]
 
-# loop = True
-# while loop:
-#     num = 0
-#     for i in langArray:
-#         print(f"{num} - {i}")
-#         num += 1
-#     try:
-#         opt = int(input("seleciona una opcion: "))
-#         print("\n")
-#     except ValueError:
-#         print("Please input integer only...")
-#         continue
-#     if len(langArray) == 1 or opt == 0:
-#         loop = False
+loop = True
+while loop:
+    num = 0
+    for i in langArray:
+        print(f"{num} - {i}")
+        num += 1
+    try:
+        opt = int(input("seleciona una opcion: "))
+        print("\n")
+    except ValueError:
+        print("Please input integer only...")
+        continue
+    if len(langArray) == 1 or opt == 0:
+        loop = False
 
-#     elif opt > len(langArray) - 1:
-#         print(WARNING + "!!Numero no valido¡¡ \n" + ENDC)
-#     else:
-#         n = langUrlArray[opt]
-#         filedata = filedata.replace('{pLang}', n + "{pLang}")
-#         langArray.pop(opt)
-#         langUrlArray.pop(opt)
-#     with open('README.md', 'w') as file:
-#         file.write(filedata)
+    elif opt > len(langArray) - 1:
+        print(WARNING + "!!Numero no valido¡¡ \n" + ENDC)
+    else:
+        n = langUrlArray[opt]
+        filedata = filedata.replace('{pLang}', n + "{pLang}")
+        langArray.pop(opt)
+        langUrlArray.pop(opt)
+    with open('README.md', 'w') as file:
+        file.write(filedata)
 
-# filedata = filedata.replace('{pLang}', "")
+filedata = filedata.replace('{pLang}', "")
 
 # Project status
 status = ['Estas trabajando en el proyecto: ','Es responsive: ','Tiene hosting: ']
@@ -95,21 +95,21 @@ while len(status) > 0:
     status.pop(0)
     with open('README.md', 'w') as file:
         file.write(filedata)
-# print("\n")
-# print("Project preview")
-# print("1 si no tienes imagen de preview")
-# print("2 si tienes imagen de preview")
-# print("3 si tienes gif de preview \n")
-# preview = input("seleciona una opcion: ")
-# if preview == "1":
-#     filedata = filedata.replace('{pPreview}', "``vista no disponible``")
-# if preview == "2":
-#     filedata = filedata.replace(
-#         '{pPreview}', "<img src=\"./preview.png\" width=\"100%\">")
-# if preview == "3":
-#     filedata = filedata.replace(
-#         '{pPreview}', "<img src=\"./preview.gif\" width=\"100%\">")
+print("\n")
+print("Project preview")
+print("1 si no tienes imagen de preview")
+print("2 si tienes imagen de preview")
+print("3 si tienes gif de preview \n")
+preview = input("seleciona una opcion: ")
+if preview == "1":
+    filedata = filedata.replace('{pPreview}', "``vista no disponible``")
+if preview == "2":
+    filedata = filedata.replace(
+        '{pPreview}', "<img src=\"./preview.png\" width=\"100%\">")
+if preview == "3":
+    filedata = filedata.replace(
+        '{pPreview}', "<img src=\"./preview.gif\" width=\"100%\">")
 with open('README.md', 'w') as file:
     file.write(filedata)
-# system("DEL Readme-gen.py && git pull && git add -A && git commit -a -m \"update\" && git push")
-# webbrowser.open("https://github.com/Carloscruzvalencia/"+ pTitle + "#readme")
+system("DEL Readme-gen.py && git pull && git add -A && git commit -a -m \"update\" && git push")
+webbrowser.open("https://github.com/Carloscruzvalencia/"+ pTitle + "#readme")
