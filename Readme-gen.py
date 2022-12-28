0f031d0efced2a826817240c942cc0deab75f4f1
Carlos cruz valencia <a2carcruzinfo@gmail.com>
2022-12-28 06:33:18 +0100

    Terminadas las modificaciones en el codigo

--- Readme-gen.py
+++ Readme-gen.py
@@ -1,58 +1,19 @@
 from os import system
 import webbrowser
-
 WARNING = '\033[31m'
 ENDC = '\033[0m'
-
 with open('README.md', 'r') as file:
     filedata = file.read()
-
-# Nombre del proyecto
 pTitle = input("Nombre del proyecto: ")
 filedata = filedata.replace("{pTitle}", pTitle)
-
-# Descripción del proyecto
 pDesc = input("Descripción del proyecto: ")
 filedata = filedata.replace('{pDesc}', pDesc)
-
-# Url del proyecto
 pUrl = "https://carloscruzvalencia.github.io/" + pTitle
 filedata = filedata.replace('{pUrl}', pUrl)
-
-# # lenguajes del proyecto
 print("\n")
 print("Que tecnologias as usado en el proyecto")
-langUrlArray = [
-    "",
-    # html
-    "<code><img height=\"30\" src=\"https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white\"></code>",
-    # css
-    "<code><img height=\"30\" src=\"https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white\"></code>",
-    # sass
-    "<code><img height=\"30\" src=\"https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white\"></code>",
-    # javascript
-    "<code><img height=\"30\" src=\"https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E\"></img></code>",
-    # python
-    "<code><img height=\"30\" src=\"https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white\"></code>",
-    # scrolltringer
-    "<code><img height=\"30\" src=\"https://img.shields.io/badge/ScrollTrigger-E34F26?style=for-the-badge&logo=scrolltrigger&logoColor=white\"></code>",
-    # bootsrap
-    "<code><img height=\"30\" src=\"https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white\"></code>",
-    # spline
-    "<code><img height=\"30\" src=\"https://i.postimg.cc/85hR92yS/Group-71.png\"></code>"
-]
-langArray = [
-    "salir",
-    "html",
-    "css",
-    "sass",
-    "javascript",
-    "python",
-    "ScrollTrigger",
-    "Bootstrap",
-    "spline"
-]
-
+langUrlArray = ["","<code><img height=\"30\" src=\"https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white\"></code>","<code><img height=\"30\" src=\"https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white\"></code>","<code><img height=\"30\" src=\"https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white\"></code>","<code><img height=\"30\" src=\"https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E\"></img></code>","<code><img height=\"30\" src=\"https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white\"></code>","<code><img height=\"30\" src=\"https://img.shields.io/badge/ScrollTrigger-E34F26?style=for-the-badge&logo=scrolltrigger&logoColor=white\"></code>","<code><img height=\"30\" src=\"https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white\"></code>","<code><img height=\"30\" src=\"https://i.postimg.cc/85hR92yS/Group-71.png\"></code>"]
+langArray = ["salir","html","css","sass","javascript","python","ScrollTrigger","Bootstrap","spline"]
 loop = True
 while loop:
     num = 0
@@ -77,10 +38,7 @@ while loop:
         langUrlArray.pop(opt)
     with open('README.md', 'w') as file:
         file.write(filedata)
-
 filedata = filedata.replace('{pLang}', "")
-
-# Project status
 status = ['Estas trabajando en el proyecto: ','Es responsive: ','Tiene hosting: ']
 while len(status) > 0:
     opt = input(status[0])
